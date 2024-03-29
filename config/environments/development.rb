0f2e19e818fb268a98c45c4c42a341ea11ccd06b
hilindst <114443718+hilindst@users.noreply.github.com>
2024-03-28 19:31:35 -0500

    correcting deployment  files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,11 +54,6 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  # Highlight code that enqueued background job in logs.
-  config.active_job.verbose_enqueue_logs = true
-
-  # Suppress logger output for asset requests.
-  config.assets.quiet = true
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
@@ -68,9 +63,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  # Raise error when a before_action's only/except options reference missing actions
-  config.action_controller.raise_on_missing_callback_actions = true
-
-  config.debug_exception_response_format = :api
 end
