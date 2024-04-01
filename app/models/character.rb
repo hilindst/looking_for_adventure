eb1fc20a93285da659a0d1823e04aea18e08acc2
hilindst <114443718+hilindst@users.noreply.github.com>
2024-04-01 18:18:21 -0500

    corrected character model trying to get column type to fix

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,8 +1,6 @@
 class Character < ApplicationRecord
   belongs_to :player
   has_many :adventures, through: :adventure_characters
-end
-
 
   validates :name, :race, :char_class, :alignment, :level, presence: true
 end
