75b5ca32902356e3c4e9836501d1ee6210dd124a
hilindst <114443718+hilindst@users.noreply.github.com>
2024-04-17 13:34:48 -0500

    trying to add images

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -1,7 +1,7 @@
 class Adventure < ApplicationRecord
   belongs_to :player
   has_many :characters, through: :adventure_characters
-
+  has_one_attached :image
 
   validates :title, :ruleset, :description, :status, :adventure_type, :setting, presence: true
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,7 @@
 class Character < ApplicationRecord
   belongs_to :player
   has_many :adventures, through: :adventure_characters
+  has_one_attached :image
 
   validates :name, :race, :char_class, :alignment, :level, presence: true
 end
